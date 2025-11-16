bb2a6990e31a051043fe3c31cf63178798c8e3aa
m <13090422+marciob@users.noreply.github.com>
2025-11-16 03:57:14 -0300

    adding empty frontend

---
+++ app/app/page.tsx
@@ -0,0 +1,10 @@
+// app/page.tsx
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+      Hello World
+    </div>
+  );
+}
